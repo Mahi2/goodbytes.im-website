5ff599464abb250955724a1b7e570796b8bd0f4a
Sabiduria <antdepadoue@gmail.com>
2019-03-26 14:46:08 +0200

    FormData updated

--- views/form-data/formdata.php
+++ views/form-data/formdata.php
@@ -11,13 +11,18 @@ if (isset($_POST) && sizeof($_POST) > 0) {
 	}
 	
 	$email_address = $_POST['email']['val'];
-	$email_subject = "Form submitted by: ".$_POST['name']['val'];
+	$email_subject = $_POST['subject']['val'];
 	$email_body    = "You have received a new message. <br/>".
 				  	 "Here are the details: <br/><br/>";
 				  	 foreach ($_POST as $key => $value) {
 				  	 	$email_body .= "<strong>" . $value['label'] . ": </strong> " . $value['val'] . "<br/><br/>";
 				  	 }
-
+    $headers = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= "Reply-To: " . $email . "\r\n";
+    $headers .= "X-Mailer: PHP/" . phpversion() . "\r\n";
+    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+    $headers .= 'X-MSMail-Priority: Normal' . "\r\n";
+    $headers .= 'X-Priority: 3' . "\r\n";
 	$headers = "From:<$email_address>\n";
 	$headers.= "Content-Type:text/html; charset=UTF-8";
 
